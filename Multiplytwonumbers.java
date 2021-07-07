b6859774074e236013daeec1255c7c10b36f2401
nasehanazeer <86603781+nasehanazeer@users.noreply.github.com>
2021-07-07 22:31:16 +0530

    Add files via upload

---
+++ Multiplytwonumbers.java
@@ -0,0 +1,30 @@
+package knitinggale;
+
+import java.util.Scanner;
+
+
+
+public class Multiplytwonumbers {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		
+
+		System.out.println("entr the value for i...");
+		
+		int i = scan.nextInt();
+		
+		System.out.println("enter value for j ....");
+		
+		int j = scan.nextInt();
+		int mul = i*j;
+		
+		System.out.println("the Multiplication of i and j is..." +  mul);
+	}	
+	}
+	
+
+	
+
+
---
+++ printaNumber.java
@@ -0,0 +1,14 @@
+package knitinggale;
+import java.util.Scanner;
+public class printaNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner reader = new Scanner( System.in);
+System.out.println("enter a number");
+int number = reader.nextInt();
+
+System.out.println("you entered: " + number);
+	}
+
+}
