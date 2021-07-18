23209e91b33d83755b835c281a4ef541278edd96
nasehanazeer <86603781+nasehanazeer@users.noreply.github.com>
2021-07-18 21:50:08 +0530

    Add files via upload

---
+++ DefaultValuesOfDataTypes.java
@@ -0,0 +1,31 @@
+package labBookWeek3;
+
+
+
+public class DefaultValuesOfDataTypes {
+	static byte b;
+    static short s;
+      static int i;
+   static    long l;
+    static float f;
+   static  double d;
+    static boolean flag;
+    static  char c;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+     
+     System.out.println("the default value of byte is" + b );
+     System.out.println("the default value of short is" + s);
+     System.out.println("the default value of int is" + i);
+     System.out.println("the default value of long is" + l);
+     System.out.println("the default value of float is"  + f);
+     System.out.println("the default value of double is" + d);
+     System.out.println("the default value of boolean is"  + flag);
+     System.out.println("the default value of char is" +  c);
+     
+     
+     
+	}
+
+}
---
+++ IntialiazationAndTypecasting3.java
@@ -0,0 +1,54 @@
+package labBookWeek3;
+
+public class IntialiazationAndTypecasting3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=100;
+		int ii=12_34_567;
+		short s=1234;
+		
+		byte b=10;
+		byte bb=125;
+		
+		long l=23214321;
+		float f=210.200f;
+		double d=100.04;
+				
+		char c='a';
+		
+		boolean boo=true;
+		boolean booo=false;
+		
+		String str="welcome";
+		//type casting 
+		int z=i*b;
+		
+		int a=z;
+		byte bi=(byte)a;
+		System.out.println(bi);
+		
+		long ll=( long)d;
+		
+		System.out.println(ll);
+		
+		int iii=(int)l;
+		System.out.println(iii);
+		
+		 //char cc=(char)int;//cannot cast char to int.
+		
+		float ff=l;
+		
+		System.out.println(ff);
+		
+	//cannot do casting boolean to int//	boolean boooo=(boolean)int;
+		
+		
+		
+		
+		
+		
+      
+	}
+
+}
